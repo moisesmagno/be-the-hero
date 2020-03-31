6bd9d6863585aa720a9a9b46dd094e50d0e08f6c
Moisés S E Aguilar <moisesmagno@gmail.com>
2020-03-30 23:46:57 -0300

    Criado o backend da aplicação.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
 const routes = require("./routes");
 const app = express();
 
 //Informa que o transito de dados via http será em json.
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,23 @@
 const express = require("express");
 
+const OngController = require("./Controllers/OngController");
+const IncidentsController = require("./Controllers/IncidentsController");
+const ProfileController = require("./Controllers/ProfileController");
+const SessionController = require("./Controllers/SessionController");
+
+const connection = require("./database/connection");
+
 const routes = express.Router();
 
-routes.get("/", (require, response) => {
-  return response.json({
-    resposta: "teste!"
-  });
-});
+routes.post("/session", SessionController.create);
+
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
+
+routes.get("/profile", ProfileController.index);
+
+routes.get("/incidents", IncidentsController.index);
+routes.post("/incidents", IncidentsController.create);
+routes.delete("/incidents/:id", IncidentsController.delete);
 
 module.exports = routes;
